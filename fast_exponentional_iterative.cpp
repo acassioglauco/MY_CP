acd677257ce441f094de20bb657802054d1b1348
Glauco Acassio <acassioglauco@gmail.com>
2024-12-20 00:41:43 -0300

    Update fast_exponentional_iterative.cpp

--- fast_exponentional_iterative.cpp
+++ fast_exponentional_iterative.cpp
@@ -2,7 +2,7 @@ constexpr int MOD = 1e9 + 7;
 
 int64_t fexp(int64_t base, int64_t exp, int MOD) {
     int64_t result = 1;
-    while (exp) {
+    while (exp > 0) {
         if (exp & 1) {
           result = result * base % MOD;
         }
