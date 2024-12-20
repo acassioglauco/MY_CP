3c505d058f042831692434f0b1a0905e3c199d7e
Glauco Acassio <acassioglauco@gmail.com>
2024-12-20 00:22:45 -0300

    Update gcd_recursive.cpp

--- gcd_recursive.cpp
+++ gcd_recursive.cpp
@@ -1,14 +1,1 @@
-constexpr int modulo = 1e9 + 7;
-
-int64_t exponenciacao_rapida(int64_t base, int64_t expoente, int modulo) {
-        int64_t resultado = 1;
-        while(expoente > 0) {
-            if(expoente % 2 != 0) {
-                resultado = (resultado * base) % modulo;
-            }
-            base = (base * base) % modulo;
-            expoente/= 2;
-        }
-        return resultado;
-}
 
