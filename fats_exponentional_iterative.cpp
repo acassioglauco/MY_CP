868f5b505fc3893276428e1566bfc40cda13a21d
Glauco Acassio <acassioglauco@gmail.com>
2024-12-20 00:18:01 -0300

    Create fats_exponentional_iterative.cpp

---
+++ fats_exponentional_iterative.cpp
@@ -0,0 +1,14 @@
+constexpr int MOD = 1e9 + 7;
+
+int64_t fexp(int64_t base, int64_t exp, int MOD) {
+    int64_t result = 1;
+    while (exp) {
+        if (exp & 1) {
+          result = result * base % MOD;
+        }
+        base = base * base % MOD;
+        exp >>= 1;
+    }
+    return result;
+}
+
